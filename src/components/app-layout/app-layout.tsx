e4a7773cf7f937bf9cacbfa7fd636416f97e70c2
Aleksandr Petrov <petrov.aleksr@gmail.com>
2024-12-17 22:52:34 +0500

    refactor: изменена базовая вёрстка

--- src/components/app-layout/app-layout.tsx
+++ src/components/app-layout/app-layout.tsx
@@ -1,15 +1,17 @@
 import styles from './app-layout.module.css';
-import Header from '../header/header';
-import Content from '../content/content';
-import Footer from '../footer/footer';
 
 const AppLayout = () => {
-
   return (
     <div className={styles.main}>
-      <Header containerClass={styles.main__container} />
-      <Content containerClass={styles.main__container} />
-      <Footer containerClass={styles.main__container} />
+      <header className={styles.main__header}>
+        <div className={styles.main__container}>Header</div>
+      </header>
+      <div className={styles.main__content}>
+        <div className={styles.main__container}>Content</div>
+      </div>
+      <footer className={styles.main__footer}>
+        <div className={styles.main__container}>Footer</div>
+      </footer>
     </div>
   );
 };
