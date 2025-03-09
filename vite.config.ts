e610c4d7cfee25961d4f363e34af2a6a0a460efe
Aleksandr Petrov <petrov.aleksr@gmail.com>
2025-03-09 23:05:54 +0500

    refactor: поправки в конфигурациях

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/template-react-project',
+  base: '/', /* путь '/template-react-project' для gh-pages */
 });
