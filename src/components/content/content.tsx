6ef6459d4532ac60966d1918d7ba931bfb06b5b0
Aleksandr Petrov <petrov.aleksr@gmail.com>
2025-03-12 22:38:05 +0500

    refactor: поправки в базовой вёрстке

---
+++ src/components/content/content.tsx
@@ -0,0 +1,11 @@
+import styles from './content.module.css';
+
+const Content = () => {
+  return (
+    <>
+      <p className={styles.title}>Content</p>
+    </>
+  );
+};
+
+export default Content;
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <>
+      <p className={styles.title}>Footer</p>
+    </>
+  );
+};
+
+export default Footer;
